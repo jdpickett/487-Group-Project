347116eac5e61f93af1f1f4f89966a4bf61c62a2
jdpickett <jdpickett2003@gmail.com>
2026-02-20 12:36:12 -0800

    Made folders for future use and implemented player.cs for player system and IGameEntity for other entities to use

--- Game1.cs
+++ Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Core;
+using Entities;
 
 namespace _487_Group_Project;
 
@@ -14,8 +15,8 @@ public class Game1 : Game
     private SimpleDrawer _drawer = null!;
     private InputState _input = null!;
 
-    // visual
-    private Vector2 _testPos;
+    // player
+    private Player _player = null!;
 
     public Game1()
     {
@@ -37,49 +38,16 @@ public class Game1 : Game
         _spriteBatch = new SpriteBatch(GraphicsDevice);
         _drawer = new SimpleDrawer(GraphicsDevice);
         _input = new InputState();
-
-        // start test object in center of playfield
-        _testPos = new Vector2(
-            GameConfig.Playfield.Center.X,
-            GameConfig.Playfield.Center.Y
-        );
+        _player = new Player(_drawer, _input);
     }
 
     protected override void Update(GameTime gameTime)
     {
-        // if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-        //     Exit();
-
-        // TODO: Add your update logic here
-
         _input.Update();
-
-        if (_input.Down(Keys.Escape))
+        if(_input.Down(Keys.Escape))
             Exit();
-
-        float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-        // Test movement using GameConfig speeds
-        bool slow = _input.Down(Keys.LeftShift);
-        float speed = slow ? GameConfig.PlayerSpeedSlow : GameConfig.PlayerSpeedNormal;
         
-        Vector2 move = Vector2.Zero;
-        
-        if (_input.Down(Keys.W) || _input.Down(Keys.Up)) move.Y -= 1;
-        if (_input.Down(Keys.S) || _input.Down(Keys.Down)) move.Y += 1;
-        if (_input.Down(Keys.A) || _input.Down(Keys.Left)) move.X -= 1;
-        if (_input.Down(Keys.D) || _input.Down(Keys.Right)) move.X += 1;
-
-        if (move != Vector2.Zero)
-            move.Normalize();
-
-        _testPos += move * speed * dt;
-
-        // Clamp inside playfield
-        _testPos = new Vector2(
-            MathHelper.Clamp(_testPos.X, GameConfig.Playfield.Left + 10, GameConfig.Playfield.Right - 10),
-            MathHelper.Clamp(_testPos.Y, GameConfig.Playfield.Top + 10, GameConfig.Playfield.Bottom - 10)
-        );
+        _player.Update(gameTime);
 
         base.Update(gameTime);
     }
@@ -87,26 +55,15 @@ public class Game1 : Game
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.Black);
-
         _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
 
         // Draw playfield boundary
-        _drawer.DrawRectOutline(_spriteBatch, GameConfig.Playfield, 3, Color.DarkGray);
+        _drawer.DrawRectOutline(_spriteBatch, GameConfig.Playfield, thickness: 3, color: Color.DarkGray);
 
-        // Draw test square (fake player)
-        var rect = new Rectangle((int)_testPos.X - 10, (int)_testPos.Y - 10, 20, 20);
-        _drawer.DrawRect(_spriteBatch, rect, Color.White);
-
-        // Draw slow-mode indicator
-        if (_input.Down(Keys.LeftShift))
-        {
-            var hit = new Rectangle((int)_testPos.X - 2, (int)_testPos.Y - 2, 4, 4);
-            _drawer.DrawRect(_spriteBatch, hit, Color.LightGray);
-        }
+        // Draw player
+        _player.Draw(_spriteBatch);
 
         _spriteBatch.End();
-
-        // TODO: Add your drawing code here
 
         base.Draw(gameTime);
     }
--- obj/Debug/net9.0/487-Group-Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/487-Group-Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("487-Group-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91db0c329f5d96106ba291d9c844d4a511ef02bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c59ecc031398fbb2174f5b9d89b69496eec1c97")]
 [assembly: System.Reflection.AssemblyProductAttribute("487-Group-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("487-Group-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
